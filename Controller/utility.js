98c1ae98f7817a94ce9ebcac20f77330c4d747f3
MohdDaniish <70433418+MohdDaniish@users.noreply.github.com>
2022-04-21 17:28:40 +0530

    rate update

--- Controller/utility.js
+++ Controller/utility.js
@@ -6,12 +6,12 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+exports.updatePrecent = async (req, res) => {
 var db = mongoose.connection;
 var Percent = db.collection('referral_percents');
-
-exports.updatePrecent = async (req, res) => {
+console.log(req.body.level1);
 user_purchase = await Percent.updateOne({
-    _id: "625e87fae0f531522da5c769"
+    _id: '625e87fae0f531522da5c769'
 }, {
     $set :{
         buying_bonus:req.body.buying,
